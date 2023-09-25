1e8d2158a456ad42ebd0edec2c0b5c1585de3f78
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-09-25 17:31:55 -0300

    test push

--- TP2/GamesHaven/js/javascript.js
+++ TP2/GamesHaven/js/javascript.js
@@ -1,16 +1,22 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", function() {
-document.querySelector(".card").addEventListener("mouseover", cambiarEstilo);
-document.querySelector(".card").addEventListener("mouseout", cambiarEstilo);
+/*document.addEventListener("DOMContentLoaded", function() {
+    let badge = document.querySelector(".gratis");
+    badge.addEventListener("mouseover",cambiarEstilo);
+    badge.addEventListener("mouseout",cambiarEstilo);
+   
+
+
 
 function cambiarEstilo(){
     let badgeGratis = document.querySelector(".badge-gratis");
-    let badge = document.querySelector(".badge");
-    let triangulo = document.querySelector(".triangulo");
-    badgeGratis.classList.toggle("hover");
-    badge.classList.toggle("badge-hover");
+    let b = document.querySelector(".badge");
+    let triangulo = document.querySelector(".triangulo-gratis");
+    b.classList.toggle("hover");
+    badgeGratis.classList.toggle("badge-hover");
     triangulo.classList.toggle("triangulo-hover");
-    
-   
 }
-});
+
+
+
+
+});*/
