15e2e968c2817c7efe92c77ccb70b58f8efd8bd1
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-01 12:40:35 -0300

    calculando cuando detener el carrusel.

--- TP2/GamesHaven/js/javascript.js
+++ TP2/GamesHaven/js/javascript.js
@@ -8,6 +8,8 @@ let currentIndex = 0;
 
 nextBtn.addEventListener('click', () => {
   currentIndex++;
+  let carouselWidth = document.querySelector('.contenedor-carrusel').scrollWidth;
+  console.log(carouselWidth);
   if (currentIndex > 7) { // Cambiar este número si deseas mostrar más o menos tarjetas a la vez
     currentIndex = 7;
   }
@@ -34,4 +36,55 @@ updateCarousel(); // Inicializar el carrusel
 
 
 
-});
+}); 
+
+/*"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselContainer = document.querySelector('.contenedor-carrusel');
+  const carousel = document.querySelector('.carrusel');
+  const prevBtn = document.querySelector('#prevBtn');
+  const nextBtn = document.querySelector('#nextBtn');
+  const cardSize = document.querySelector('.card').offsetWidth;
+  
+  
+  let currentIndex = 0;
+  let cardWidth; // Ancho de cada tarjeta en porcentaje, se calculará dinámicamente
+  
+  nextBtn.addEventListener('click', () => {
+    currentIndex++;
+    const maxIndex = Math.max(0, Math.min(cardCount - visibleCards, cardCount - 1));
+    currentIndex = Math.min(currentIndex, maxIndex);
+    updateCarousel();
+  });
+  
+  prevBtn.addEventListener('click', () => {
+    currentIndex--;
+    currentIndex = Math.max(currentIndex, 0);
+    updateCarousel();
+  });
+  
+  function updateCarousel() {
+    cardWidth = 100 / visibleCards; // Calcula el ancho de la tarjeta dinámicamente
+    const translateX = currentIndex * -cardWidth;
+    carousel.style.transform = `translateX(${translateX}%)`;
+  }
+  
+  // Detectar el número de tarjetas visibles en función del ancho de la ventana gráfica
+  let visibleCards = calculateVisibleCards();
+  window.addEventListener('resize', () => {
+    visibleCards = calculateVisibleCards();
+    updateCarousel();
+  });
+  
+  function calculateVisibleCards() {
+    const windowWidth = window.innerWidth;
+    const cardMargin = 5; // Márgenes entre tarjetas en píxeles
+    const minCardWidth = cardSize; // Ancho mínimo de la tarjeta en píxeles
+    return Math.floor((windowWidth - cardMargin) / minCardWidth);
+  }
+  
+  // Inicializar el carrusel
+  const cardCount = document.querySelectorAll('.card').length;
+  updateCarousel();
+
+});*/
