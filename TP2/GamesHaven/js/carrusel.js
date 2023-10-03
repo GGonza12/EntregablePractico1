fc5cf724a785fb4ae6f22f9b75bbfdf835f6dca1
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-02 22:02:42 -0300

    script de carrusel reutilizable terminado.

--- TP2/GamesHaven/js/carrusel.js
+++ TP2/GamesHaven/js/carrusel.js
@@ -1,66 +1,80 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
 
-let nextBtns = document.querySelectorAll('#nextBtn');
-for(let i=0 ;i< nextBtns.length;i++){
-  nextBtns[i].addEventListener('click', siguiente);
-} ;
-let prevBtns = document.querySelectorAll('#prevBtn');
-for(let i=0 ;i< nextBtns.length;i++){
-  prevBtns[i].addEventListener('click', anterior);
-} ;
+  let indexCarruseles = [];
+
+  let nextBtns = document.querySelectorAll('#nextBtn');
+  for (let i = 0; i < nextBtns.length; i++) {
+    nextBtns[i].addEventListener('click', siguiente);
+    indexCarruseles[i] = 0;
+    nextBtns[i].dataset.index = i;
+  };
+  let prevBtns = document.querySelectorAll('#prevBtn');
+  for (let i = 0; i < nextBtns.length; i++) {
+    prevBtns[i].addEventListener('click', anterior);
+    indexCarruseles[i] = 0;
+    prevBtns[i].dataset.index = i;
+    
+  };
 
 
-let currentIndex = 0;
 
-
-function siguiente() {
-   
-    currentIndex++;
-    let claseCards= this.dataset.cards;
+  function siguiente() {
+    indexCarruseles[this.dataset.index] = indexCarruseles[this.dataset.index] + 1;
+    let currentIndex = indexCarruseles[this.dataset.index];
+    console.log(currentIndex);
+    
+    
+    let claseCards = this.dataset.cards;
     let claseCarrusel = this.dataset.carrusel;
-    let tamCard = document.querySelector(`.${claseCards}`).offsetWidth + 10 ; // el tamaño de la card + 10 del margin que tiene  
+    let tamCard = document.querySelector(`.${claseCards}`).offsetWidth + 10; // el tamaño de la card + 10 del margin que tiene  
     let cantCards = document.querySelectorAll(`.${claseCards}`).length; //cantidad de cards
     let carrusel = document.querySelector(`.${claseCarrusel}`); //carrusel
     let tamCarrusel = document.querySelector(`.${claseCarrusel}`).offsetWidth; // tamaño del carrusel que tiene en el momento
     let carouselWidth = document.querySelector(`.${claseCarrusel}`).scrollWidth;
     console.log(carouselWidth);
-    let cardsVisibles = (tamCarrusel/tamCard);
-    let limiteClick = (cantCards-cardsVisibles);
+    let cardsVisibles = (tamCarrusel / tamCard);
+    let limiteClick = (cantCards - cardsVisibles);
     if (currentIndex > (limiteClick)) { // Cambiar este número si deseas mostrar más o menos tarjetas a la vez
       currentIndex = limiteClick;
+      indexCarruseles[this.dataset.index] = limiteClick;
     }
-    updateCarousel(carrusel,tamCard,currentIndex);
+    updateCarousel(carrusel, tamCard, currentIndex);
   };
 
-function anterior(){
-  currentIndex--;
-  let claseCards= this.dataset.cards;
-  let claseCarrusel = this.dataset.carrusel;
-  let carrusel = document.querySelector(`.${claseCarrusel}`); //carrusel.
-  let tamCard = document.querySelector(`.${claseCards}`).offsetWidth + 10 ;
-  
-  if (currentIndex < 0) {
-    currentIndex = 0;
-  }
-  updateCarousel(carrusel,tamCard ,currentIndex);
-};
+  function anterior() {
+    indexCarruseles[this.dataset.index] = indexCarruseles[this.dataset.index]-1;
+    let currentIndex = indexCarruseles[this.dataset.index];
+    
+    
+    let claseCards = this.dataset.cards;
+    let claseCarrusel = this.dataset.carrusel;
+    let carrusel = document.querySelector(`.${claseCarrusel}`); //carrusel.
+    let tamCard = document.querySelector(`.${claseCards}`).offsetWidth + 10;
 
-function updateCarousel(carrusel, tamCard, currentIndex) {
-  let translateX = currentIndex * -tamCard; // lo que se tiene que mover el carrusel dependiendo de la pantalla
-  carrusel.style.transform = `translateX(${translateX}px)`;
-  console.log(carrusel);
-     
-};
+    if (currentIndex < 0) {
+      currentIndex = 0;
+      indexCarruseles[this.dataset.index] =0;
+    }
+    updateCarousel(carrusel, tamCard, currentIndex);
+  };
 
 
+  function updateCarousel(carrusel, tamCard, currentIndex) {
+    let translateX = currentIndex * -tamCard; // lo que se tiene que mover el carrusel dependiendo de la pantalla
+    carrusel.style.transform = `translateX(${translateX}px)`;
+    console.log(carrusel);
+
+  };
 
 
 
 
-//cantVisible variable que es la cantidad de cards que quiere que se muestre en el home (capaz que en px del tamaño de card)
-//cantCards guardo la cantidad de cards que tengo 
-//cantCards-cantVisible la cantidad de veces que se va a mover el carrusel si tengo 7 cards y muestro 5 se mueve 2 veces.
-//translateX mover la card por px mejor mover una card entera (230px)
+
+
+  //cantVisible variable que es la cantidad de cards que quiere que se muestre en el home (capaz que en px del tamaño de card)
+  //cantCards guardo la cantidad de cards que tengo 
+  //cantCards-cantVisible la cantidad de veces que se va a mover el carrusel si tengo 7 cards y muestro 5 se mueve 2 veces.
+  //translateX mover la card por px mejor mover una card entera (230px)
 
 }); 
