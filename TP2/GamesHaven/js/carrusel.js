7a0b63ec6e1289046a4df52fb832f897fb65e850
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-03 15:19:20 -0300

    Categorias agregadas.

--- TP2/GamesHaven/js/carrusel.js
+++ TP2/GamesHaven/js/carrusel.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
   for (let i = 0; i < nextBtns.length; i++) {
     nextBtns[i].addEventListener('click', siguiente);
     indexCarruseles[i] = 0;
-    nextBtns[i].dataset.index = i;
+    nextBtns[i].dataset.index = i; //le da posicion el arreglo al que pertenece el boton
   };
   let prevBtns = document.querySelectorAll('#prevBtn');
   for (let i = 0; i < nextBtns.length; i++) {
     prevBtns[i].addEventListener('click', anterior);
     indexCarruseles[i] = 0;
     prevBtns[i].dataset.index = i;
-    
+
   };
 
 
@@ -22,20 +22,15 @@ document.addEventListener("DOMContentLoaded", function () {
   function siguiente() {
     indexCarruseles[this.dataset.index] = indexCarruseles[this.dataset.index] + 1;
     let currentIndex = indexCarruseles[this.dataset.index];
-    console.log(currentIndex);
-    
-    
     let claseCards = this.dataset.cards;
     let claseCarrusel = this.dataset.carrusel;
     let tamCard = document.querySelector(`.${claseCards}`).offsetWidth + 10; // el tamaño de la card + 10 del margin que tiene  
     let cantCards = document.querySelectorAll(`.${claseCards}`).length; //cantidad de cards
     let carrusel = document.querySelector(`.${claseCarrusel}`); //carrusel
     let tamCarrusel = document.querySelector(`.${claseCarrusel}`).offsetWidth; // tamaño del carrusel que tiene en el momento
-    let carouselWidth = document.querySelector(`.${claseCarrusel}`).scrollWidth;
-    console.log(carouselWidth);
-    let cardsVisibles = (tamCarrusel / tamCard);
-    let limiteClick = (cantCards - cardsVisibles);
-    if (currentIndex > (limiteClick)) { // Cambiar este número si deseas mostrar más o menos tarjetas a la vez
+    let cardsVisibles = (tamCarrusel / tamCard); //la cantidad de cards que se ve en pantalla
+    let limiteClick = (cantCards - cardsVisibles); // para sacar cuando tiene que parar el carrusel
+    if (currentIndex > (limiteClick)) { 
       currentIndex = limiteClick;
       indexCarruseles[this.dataset.index] = limiteClick;
     }
@@ -43,10 +38,10 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   function anterior() {
-    indexCarruseles[this.dataset.index] = indexCarruseles[this.dataset.index]-1;
+    indexCarruseles[this.dataset.index] = indexCarruseles[this.dataset.index] - 1;
     let currentIndex = indexCarruseles[this.dataset.index];
-    
-    
+
+
     let claseCards = this.dataset.cards;
     let claseCarrusel = this.dataset.carrusel;
     let carrusel = document.querySelector(`.${claseCarrusel}`); //carrusel.
@@ -54,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (currentIndex < 0) {
       currentIndex = 0;
-      indexCarruseles[this.dataset.index] =0;
+      indexCarruseles[this.dataset.index] = 0;
     }
     updateCarousel(carrusel, tamCard, currentIndex);
   };
@@ -63,7 +58,6 @@ document.addEventListener("DOMContentLoaded", function () {
   function updateCarousel(carrusel, tamCard, currentIndex) {
     let translateX = currentIndex * -tamCard; // lo que se tiene que mover el carrusel dependiendo de la pantalla
     carrusel.style.transform = `translateX(${translateX}px)`;
-    console.log(carrusel);
 
   };
 
