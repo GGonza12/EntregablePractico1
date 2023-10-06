919d81d5b4eebf2c0d56ed32ae03c40ab7b01cc3
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-06 17:25:23 -0300

    botones añadidos

---
+++ TP2/GamesHaven/js/carrito.js
@@ -0,0 +1,24 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    let btn = document.querySelectorAll(".btnCarrito");
+    for(let i=0;i<btn.length;i++){
+        btn[i].addEventListener("click",agregar);
+        btn[i].dataset.index = i;
+
+    }
+    let estadoCarrito = document.querySelectorAll(".agregadoCarritoOculto");
+
+    for(let i=0;i<estadoCarrito.length;i++){
+        estadoCarrito[i].dataset.index = i;
+    }
+
+
+    function agregar(){
+        let pos = this.dataset.index;
+        btn[pos].classList.toggle("sacar");
+        estadoCarrito[pos].classList.toggle("mostrar");
+
+    }
+
+
+});
