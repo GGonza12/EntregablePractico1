9d1c3437187f04f18a9bb0bbe323662288ef6dec
Juan Simon Zubigaray <zubigarayjuansimon@gmail.com>
2023-10-08 11:40:18 -0300

    Barra de carga y varias cosas añadidas

--- TP2/GamesHaven/js/overlay.js
+++ TP2/GamesHaven/js/overlay.js
@@ -1,32 +1,7 @@
 "use strict";
 
-
-/*Como que se va difuminando cuando pasan los 5 segundos */
 document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-        document.querySelector('.overlay').style.opacity = 0;
-    }, 5000);
+  setTimeout(function () {
+    document.querySelector('.overlay').style.opacity = 0;
+  }, 5000);
 });
-
-
-/*Se elimina de una sin esperar a que se  */
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    // Bloquear la interacción del usuario
-    document.body.style.pointerEvents = 'none';
-  
-    // Esperar 5 segundos y luego cambiar la opacidad del overlay
-    setTimeout(function () {
-      var overlay = document.querySelector('.overlay');
-      if (overlay) {
-        // Eliminar el overlay del DOM
-        overlay.remove();
-      }
-  
-      // Desbloquear la interacción del usuario después de que haya transcurrido el tiempo
-      setTimeout(function () {
-        document.body.style.pointerEvents = 'auto';
-      }, 500);
-    }, 5000);
-  });
-  */
