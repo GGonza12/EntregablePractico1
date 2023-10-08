4c2a5b6f71cab13a4bc07f6a2df65a66f3c60aa4
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-08 19:59:46 -0300

    mobile terminado y script arreglado

--- TP2/GamesHaven/js/overlay.js
+++ TP2/GamesHaven/js/overlay.js
@@ -4,4 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   setTimeout(function () {
     document.querySelector('.overlay').style.opacity = 0;
   }, 5000);
+  setTimeout(function () {
+    document.querySelector('.overlay').remove();
+  }, 5100);
 });
