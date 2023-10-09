1efcf779d180ec0ff4e5bfa44915e2a1f95ccfea
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-09 16:12:26 -0300

    carrito terminado pero sin funcionar

--- TP2/GamesHaven/js/desplegable.js
+++ TP2/GamesHaven/js/desplegable.js
@@ -1,6 +1,7 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".lupa").addEventListener("click",busquedaVisible);
+    document.querySelector(".carritoCart").addEventListener("click",carritoVisible);
 
     function busquedaVisible() {
         let busqueda = document.querySelector(".contenedorBarraBusqueda");
@@ -9,5 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
         
 
     };
+    function carritoVisible(){
+        console.log("ja");
+        let carrito = document.querySelector(".shopping-cart");
+        carrito.classList.toggle("shopping-cart-Visible");
+        console.log("ja");
+    }
+
 
 });
