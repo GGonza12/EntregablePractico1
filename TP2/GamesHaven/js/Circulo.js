ee92594737300149dce16d9f624fd3cfffe89bf8
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-10-27 20:45:52 -0300

    fichas agregadas

--- TP2/GamesHaven/js/Circulo.js
+++ TP2/GamesHaven/js/Circulo.js
@@ -13,7 +13,46 @@ class Circulo extends Figura {
         this.context.closePath();
         
     }
-
+    drawImage(){
+        let imgFicha = new Image();
+        imgFicha.src = this.fill;
+        let drawFicha = ()=> {
+            this.context.save();
+            this.context.beginPath();
+            this.context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+            this.context.stroke();
+            this.context.closePath();
+            this.context.clip();
+           
+            this.context.drawImage(imgFicha, (this.posX-this.radius), (this.posY-this.radius), this.radius*2,this.radius*2);
+        
+            this.context.beginPath();
+            this.context.arc(this.posX, this.posY-50, this.radius, 0, 2 * Math.PI);
+            this.context.clip();
+            this.context.closePath();
+            this.context.restore();
+           
+        }; 
+        imgFicha.onload =  drawFicha;
+    }
+/*
+    drawImage(){
+        this.context.save();
+        this.context.beginPath();
+        this.context.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+        let imgFicha = new Image();
+        imgFicha.src = this.fill; 
+        let draw = ()=>this.context.drawImage(imgFicha, this.posX, this.posY, this.radius*2, this.radius*2);
+  
+        imgFicha.onload = draw;
+        imgFicha.onload = function() {
+        this.context.drawImage(imgFicha, this.posX, this.posY);
+        
+    }; 
+    this.context.closePath();
+    
+    }
+*/
     getRadius() {
         return this.radius;
     }
--- TP2/GamesHaven/js/tablero.js
+++ TP2/GamesHaven/js/tablero.js
@@ -1,53 +1,112 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-    let canvasWidth=1920;
-    let canvasHeight=1080;
-    let limiteWidth= 7;
-    let limiteHeight =6;
-    let juegoWidth= 1024;
-    let juegoHeight = 768;
-    
+    let canvasWidth = 1920;
+    let canvasHeight = 1080;
+    let limiteWidth = 7;
+    let limiteHeight = 6;
+    let juegoWidth = 800;
+    let juegoHeight = 600;
+    let maximoFichas = limiteWidth * limiteHeight;
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
     let huecos = [];
+    let fichas = [];
 
+    let imgFichas = ["./images/paginaJuego/TT1.png",
+        "./images/paginaJuego/TT2.png",
+        "./images/paginaJuego/TT3.png",
+        "./images/paginaJuego/TT4.png",
+        "./images/paginaJuego/CT1.png",
+        "./images/paginaJuego/CT2.png",
+        "./images/paginaJuego/CT3.png",
+        "./images/paginaJuego/CT4.png"];
     function probarTablero() {
 
-        addRectangulo();
-        addRectangulo2();
+        addRectanguloFondo();
+        addRectanguloJuego();
+        addRectangulo(canvasWidth / 7, 100, 150, 700, "#5d79ae");
+        console.log(canvasWidth / 20);
+        addRectangulo(1500, 100, 150, 700, "#413a27");
         console.log(huecos);
-        let y= (juegoWidth/7)+80;
-        for(let i=0;i<limiteHeight;i++){
+        let y = (canvasHeight / 5) + 60;
+        //let y= (juegoWidth/5)+20;
+        // addFicha(500,y,(((juegoWidth/2)/7)-15),ruta);
 
-            let x= (juegoWidth/2)+70;
-            for(let j=0;j<limiteWidth;j++){
-                
-                addHueco(x,y,(((juegoWidth/2)/7)-15),"#101B27",ctx);
-                x = (x+((juegoWidth)/7)-10);
+
+
+        for (let i = 0; i < limiteHeight; i++) {
+
+            let x = (juegoWidth / 2) + 170;
+            for (let j = 0; j < limiteWidth; j++) {
+
+                addHueco(x, y, (40), "#101B27", ctx);
+                x = (x + ((juegoWidth) / 7) - 10);
             };
-            y = y+(juegoHeight/7)+25;
-          
+            y = y + (juegoHeight / 7) + 10;
+
         };
-        for(let c=0;c<huecos.length;c++){
+        for (let c = 0; c < huecos.length; c++) {
             huecos[c].draw();
         }
-    };
 
-    function addRectangulo() {
-        let fondo = new Rectangulo(0, 0, 1920, 1080, "#101B27", ctx);
+        generarFichas(y);
+        
+        //  let ruta = "./images/paginaJuego/CT1.png";
+        //  addFicha((canvasWidth/7)+75,y-150,(40),ruta);
+        //  fichas[0].drawImage();
+
+    };
+    function addRectangulo(x, y, width, height, color) {
+        let fondo = new Rectangulo(x, y, width, height, color, ctx);
         fondo.draw();
 
 
     };
-    function addRectangulo2() {
-        let fondo2 = new Rectangulo((1920/4), (1080/13), 1024, 900, "#273849", ctx);
+    function addRectanguloFondo() {
+        let fondo = new Rectangulo(0, 0, canvasWidth, canvasHeight, "#101B27", ctx);
+        fondo.draw();
+
+
+    };
+    function addRectanguloJuego() {
+        let fondo2 = new Rectangulo((canvasWidth / 4), canvasHeight / 5, juegoWidth, juegoHeight, "#273849", ctx);
         fondo2.draw();
     };
-    function addHueco(posX,posY,radio,color){
-        let circulo = new Circulo(posX,posY,radio,color,ctx);
-        huecos.push(circulo);    
+    function addHueco(posX, posY, radio, color) {
+        let circulo = new Circulo(posX, posY, radio, color, ctx);
+        huecos.push(circulo);
     };
+
+    function addFicha(posX, posY, radio, imgRuta) {
+        //  ct1.src = "./images/paginaJuego/CT1.png";
+        let circulo = new Circulo(posX, posY, radio, imgRuta, ctx);
+        fichas.push(circulo);
+
+    };
+
+    function generarFichas(y){
+        let yFicha = y;
+        for (let f = 0; f < maximoFichas; f++) {
+            if(f<maximoFichas/2){
+                let ruta = imgFichas[Math.round(Math.random() * 3)];
+                addFicha((canvasWidth / 7) + 75, yFicha - 150, (40), ruta);
+            }
+            else {
+                
+                if(f==maximoFichas/2){
+                    yFicha = y;
+                }
+                let ruta = imgFichas[Math.round(Math.random() * (imgFichas.length-4)+4)];
+                addFicha(1600, yFicha - 150, (40), ruta);
+            }
+        
+       
+            fichas[f].drawImage();
+            yFicha = yFicha - 60;
+
+        };
+    }
 
     probarTablero();
 
