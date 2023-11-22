82e4ed2b4e7c470eaff98e4f602250e262aa7db6
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-11-21 22:04:08 -0300

    intento del opcional 5

---
+++ TP3/js/hoverHeroes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelector("spider-gwen-stacy").addEventListener("mouseover",fondoGwen);
+    document.querySelector("spider-peter-parker").addEventListener("mouseover",fondoPeter);
+    document.querySelector("spider-miles-morales").addEventListener("mouseover",fondoMiles);
+    let fondo=document.querySelector("rectangle-10");
+    function fondoGwen(){
+        
+    }
+
+    function fondoPeter(){
+
+    }
+
+    function fondoMiles(){
+
+    }
+
+
+
+
+});
