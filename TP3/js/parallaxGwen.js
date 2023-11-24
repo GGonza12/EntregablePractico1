d8d36541099eaf576a01e3aee2ab2d0a5773a5b7
Gonzalo Gomez <gonza12ariel@gmail.com>
2023-11-24 18:53:19 -0300

    ejercicio 11 terminado

--- TP3/js/parallaxGwen.js
+++ TP3/js/parallaxGwen.js
@@ -3,16 +3,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-let ghostSpider = document.querySelector(".ghost-spider");
 let paralx0 = document.querySelector(".cardx0");
 let paralx1 = document.querySelector(".cardx1");
 let paralx2 = document.querySelector(".cardx2");
+window.onscroll = function () {
 
-ghostSpider.onscroll = function () {
-  let y = ghostSpider.scrollTop;
-  paralx0.style.top = 1800 - y * 2.65 + "px";
-  paralx1.style.top = 1200 - y * 0.75 + "px";
-  paralx2.style.top = 800 + y * 0.5 + "px";
+  let y = window.scrollY;
+  paralx0.style.top = 200 - y * 0.15 + "px";
+  paralx1.style.top = 70 - y * 0.13 + "px";
+  paralx2.style.top = -100 - y * 0.1 + "px";
 };
 
 
